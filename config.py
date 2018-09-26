4e5234b8fc86d85b1a34b8279a869a808af8cff9
Savva Morozov <pythontheback@gmail.com>
2018-09-26 17:41:52 +0300

    Version of the program on the end of the 3rd chapter

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class Configuration():
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ scripts/create_db.py
@@ -0,0 +1,7 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+from main import db  # noqa
+
+if __name__ == '__main__':
+    db.create_all()
