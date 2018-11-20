69ab17a2b7c866ffb7365458ab5c3f4bdf869f39
Savva Morozov <pythontheback@gmail.com>
2018-11-20 17:08:45 +0300

    Add ability to edit entries via form

--- entries/blueprint.py
+++ entries/blueprint.py
@@ -59,6 +59,22 @@ def detail(slug):
     return render_template('entries/detail.html', entry=entry)
 
 
+@entries.route('/<slug>/edit/', methods=['GET', 'POST'])
+def edit(slug):
+    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    if request.method == 'POST':
+        form = EntryForm(request.form, obj=entry)
+        if form.validate():
+            entry = form.save_entry(entry)
+            db.session.add(entry)
+            db.session.commit()
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm(obj=entry)
+
+    return render_template('entries/edit.html', entry=entry, form=form)
+
+
 def entry_list(template, query, **context):
     search = request.args.get('q')
     if search:
